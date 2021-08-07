43554d4508f0f1f11e53c38102b026de84af0615
Pedram MohammadAliPour <pedramXIII@gmail.com>
2021-08-07 20:13:13 +0430

    add null-safety

--- test/flutter_pusher_test.dart
+++ test/flutter_pusher_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_pusher_client/flutter_pusher.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_pusher');
