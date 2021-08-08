5f1dea6727664322d28f12d2022b49b96378ca2a
Mohammad Pourskandar <mohammadpourskandar>
2021-08-08 18:30:59 +0430

    back to v 8.0

--- ios/Classes/UserAgent.h
+++ ios/Classes/UserAgent.h
@@ -1,4 +1,4 @@
 // Generated file, do not edit
-#define LIBRARY_VERSION @"1.0.4"
+#define LIBRARY_VERSION @"1.0.1"
 #define LIBRARY_NAME @"flutter_pusher_client"
-#define PUSHER_LIBRARY_VERSION @"~> 9.0"
+#define PUSHER_LIBRARY_VERSION @"~> 8.0"
