ef6ba29b15780f415ec8cb1616d1e948e1fd035f
Mohammad Pourskandar <mohammadpourskandar>
2021-08-08 18:23:05 +0430

    fix build problem

--- ios/Classes/UserAgent.h
+++ ios/Classes/UserAgent.h
@@ -1,4 +1,4 @@
 // Generated file, do not edit
-#define LIBRARY_VERSION @"1.0.3"
+#define LIBRARY_VERSION @"1.0.4"
 #define LIBRARY_NAME @"flutter_pusher_client"
 #define PUSHER_LIBRARY_VERSION @"~> 9.0"
