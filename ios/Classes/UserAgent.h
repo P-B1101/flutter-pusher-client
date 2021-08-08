b18c9f09628fcb403f880c7c46df1229eaf72698
Mohammad Pourskandar <mohammadpourskandar>
2021-08-08 16:10:16 +0430

    remove encrypted arg

--- ios/Classes/UserAgent.h
+++ ios/Classes/UserAgent.h
@@ -1,4 +1,4 @@
 // Generated file, do not edit
-#define LIBRARY_VERSION @"0.3.1"
+#define LIBRARY_VERSION @"1.0.1"
 #define LIBRARY_NAME @"flutter_pusher_client"
 #define PUSHER_LIBRARY_VERSION @"~> 8.0"
